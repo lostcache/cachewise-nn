a9946ba66fa26f5d08a2640e3e6130712e153265
Harsh Devisha <harshdevisha9111@gmail.com>
2025-01-06 20:29:55 -0800

    refactor(MatSys) moved to separate file

--- src/root.zig
+++ src/root.zig
@@ -1,46 +1,6 @@
 const std = @import("std");
-const testing = std.testing;
-const Allocator = std.mem.Allocator;
-const ArrayList = std.ArrayList;
+pub const MatSys = @import("./matsys.zig").MatSys;
 
-pub fn MatSys(comptime T: type) type {
-    return MatSysAligned(T);
-}
-
-fn MatSysAligned(comptime T: type) type {
-    return struct {
-        const Self = @This();
-
-        allocator: Allocator,
-
-        data: ArrayList(T),
-
-        pub fn initCapacity(alloc: Allocator, init_cap: usize) !Self {
-            return Self{ .allocator = alloc, .data = try ArrayList(T).initCapacity(alloc, init_cap) };
-        }
-
-        pub fn init(alloc: Allocator) !Self {
-            return Self{ .allocator = alloc, .data = ArrayList(T).init(alloc) };
-        }
-
-        pub fn deinit(self: *Self) void {
-            self.data.deinit();
-        }
-    };
-}
-
-test "initCapacity" {
-    const alloc = std.testing.allocator;
-    var mat = try MatSys(i32).initCapacity(alloc, 10);
-    defer mat.deinit();
-    try mat.data.append(42);
-    try testing.expectEqual(@as(i32, 42), mat.data.pop());
-}
-
-test "init" {
-    const alloc = std.testing.allocator;
-    var mat = try MatSys(i32).init(alloc);
-    defer mat.deinit();
-    try mat.data.append(42);
-    try testing.expectEqual(@as(i32, 42), mat.data.pop());
+test {
+    std.testing.refAllDecls(@This());
 }
