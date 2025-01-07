2443284f553da8cf038e4f90086537c2d6b363a6
Harsh Devisha <harshdevisha9111@gmail.com>
2025-01-07 15:15:45 -0800

    feat(MatSys) add from slice

--- src/matsys.zig
+++ src/matsys.zig
@@ -26,21 +26,41 @@ fn MatSysAligned(comptime T: type) type {
         pub fn deinit(self: *Self) void {
             self.data.deinit();
         }
+
+        // adds a matrix to the system from a slice.
+        // first creates space in the existing system with size of the slice.
+        // then does a memcpy.
+        // The operation is O(N).
+        pub fn addFromSlice(self: *Self, slice: []const T) !void {
+            const currLen = self.data.items.len;
+            try self.data.insertSlice(currLen, slice);
+        }
     };
 }
 
 test "initCapacity" {
     const alloc = std.testing.allocator;
-    var mat = try MatSys(i32).initCapacity(alloc, 10);
-    defer mat.deinit();
-    try mat.data.append(42);
-    try testing.expectEqual(@as(i32, 42), mat.data.pop());
+    var matSys = try MatSys(i32).initCapacity(alloc, 10);
+    defer matSys.deinit();
+    try matSys.data.append(42);
+    try testing.expectEqual(@as(i32, 42), matSys.data.pop());
 }
 
 test "init" {
     const alloc = std.testing.allocator;
-    var mat = try MatSys(i32).init(alloc);
-    defer mat.deinit();
-    try mat.data.append(42);
-    try testing.expectEqual(@as(i32, 42), mat.data.pop());
+    var matSys = try MatSys(i32).init(alloc);
+    defer matSys.deinit();
+    try matSys.data.append(42);
+    try testing.expectEqual(@as(i32, 42), matSys.data.pop());
+}
+
+test "add from slice" {
+    const alloc = std.testing.allocator;
+    var matSys = try MatSys(i32).init(alloc);
+    defer matSys.deinit();
+    const slice = [_]i32{ 1, 2, 3 };
+    try matSys.addFromSlice(&slice);
+    try testing.expectEqual(@as(i32, 1), matSys.data.items[0]);
+    try testing.expectEqual(@as(i32, 2), matSys.data.items[1]);
+    try testing.expectEqual(@as(i32, 3), matSys.data.items[2]);
 }
