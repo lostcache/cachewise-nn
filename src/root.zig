af6f7cd1151705f09cf1eaf2b3b145e77536ce0d
Harsh Devisha <harshdevisha9111@gmail.com>
2025-01-06 16:42:24 -0800

    feat(MatSys): init, deinit, initCapacity.

--- src/root.zig
+++ src/root.zig
@@ -1,10 +1,46 @@
 const std = @import("std");
 const testing = std.testing;
+const Allocator = std.mem.Allocator;
+const ArrayList = std.ArrayList;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
+pub fn MatSys(comptime T: type) type {
+    return MatSysAligned(T);
 }
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+fn MatSysAligned(comptime T: type) type {
+    return struct {
+        const Self = @This();
+
+        allocator: Allocator,
+
+        data: ArrayList(T),
+
+        pub fn initCapacity(alloc: Allocator, init_cap: usize) !Self {
+            return Self{ .allocator = alloc, .data = try ArrayList(T).initCapacity(alloc, init_cap) };
+        }
+
+        pub fn init(alloc: Allocator) !Self {
+            return Self{ .allocator = alloc, .data = ArrayList(T).init(alloc) };
+        }
+
+        pub fn deinit(self: *Self) void {
+            self.data.deinit();
+        }
+    };
+}
+
+test "initCapacity" {
+    const alloc = std.testing.allocator;
+    var mat = try MatSys(i32).initCapacity(alloc, 10);
+    defer mat.deinit();
+    try mat.data.append(42);
+    try testing.expectEqual(@as(i32, 42), mat.data.pop());
+}
+
+test "init" {
+    const alloc = std.testing.allocator;
+    var mat = try MatSys(i32).init(alloc);
+    defer mat.deinit();
+    try mat.data.append(42);
+    try testing.expectEqual(@as(i32, 42), mat.data.pop());
 }
